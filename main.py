d799597f1b917865abc5994f538fc9bc7fda024d
Cristina Martinez <maria.martinez4724@alumnos.udg.mx>
2020-11-19 01:21:46 -0600

    Interfaz

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from PySide2.QtWidgets import QApplication
 from mainwindow import MainWindow
-import sys
+import sys 
 
 app = QApplication()
 
 window = MainWindow()
 
 window.show()
-
 sys.exit(app.exec_())
